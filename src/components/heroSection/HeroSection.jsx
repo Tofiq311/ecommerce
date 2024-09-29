0ca707131e71bea4b452c550e60d5866b1970e65
Tofiq311 <ashik.tofiq.1@gmail.com>
2024-09-29 10:59:06 +0900

    new commit

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
     return (
         <div>
-           <img className=" h-40 lg:h-full " src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
+           <img className=" h-40 lg:h-full lg:w-full " src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
         </div>
     );
 }
